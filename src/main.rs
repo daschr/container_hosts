25037b30ee795296b54c2c3bd0a63339aafa64be
David Schramm <david.schramm@live.com>
2023-09-13 15:00:52 +0200

    add args

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use std::env;
 use std::io::Error as IoError;
-use std::path::Path;
 use std::process::exit;
+
+use clap::Parser;
 
 mod docker;
 mod hosts;
 use hosts::Hosts;
 
-const DOCKER_SOCKET: &str = "/var/run/docker.sock";
-const HOSTS: &str = "/etc/hosts";
+/// Generates /etc/hosts entries for all docker containers
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about,
+    long_about = None
+)]
+struct Args {
+    /// Unix socket path to docker
+    #[arg(long, default_value_t = String::from("/var/run/docker.sock"))]
+    docker_socket: String,
+
+    /// Path to hosts
+    #[arg(long, default_value_t = String::from("/etc/hosts"))]
+    hosts: String,
+}
 
 fn main() -> Result<(), IoError> {
-    let lister = docker::ContainerLister::new(DOCKER_SOCKET);
+    let args = Args::parse();
+
+    let lister = docker::ContainerLister::new(args.docker_socket.as_str());
     let container_entries: Vec<String> = match lister.fetch() {
         Ok(v) => v
             .iter()
@@ -27,7 +43,7 @@ fn main() -> Result<(), IoError> {
         exit(0);
     }
 
-    let mut hosts = match Hosts::new(HOSTS) {
+    let mut hosts = match Hosts::new(args.hosts) {
         Ok(v) => v,
         Err(e) => {
             eprintln!("Error: could not open hosts: {:?}", e);
